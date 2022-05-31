42ec5ddf8168dc159941b67295638b79df74cf2c
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-05-30 22:53:22 -0700

    Mostly Ui changes made to the app. Looks a lot cleaner now

---
+++ Components/CategoryGridTile.jsx
@@ -0,0 +1,57 @@
+import { View, Pressable, Text, StyleSheet, Platform } from "react-native";
+
+// I expect to get the title as prop
+const CategoryGridTile = ({ title, color }) => {
+  return (
+    <View style={[styles.gridItem]}>
+      {/* for pressable component, we get a pressed value from the style prop */}
+      {/* this tells us whether the component is actively pressed or not */}
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed && styles.buttonPressed,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    backgroundColor: "white",
+    margin: 16,
+    height: 150,
+    borderRadius: 10,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.select({ ios: "none", android: "hidden" }),
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.4,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
+
+export default CategoryGridTile;
---
+++ screens/CategoriesScreen.jsx
@@ -0,0 +1,35 @@
+import { FlatList, StyleSheet } from "react-native";
+
+import { CATEGORIES } from "../data/dummydata";
+import CategoryGridTile from "../Components/CategoryGridTile";
+
+// when we use renderItem in a flatList - we get the itemData as a value
+// but this itemData is not the item itself, it is a much bigger object
+// the actual individual items are located in itemData.item!
+const renderCategoryItem = (itemData) => {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+};
+
+const CategoriesScreen = () => {
+  return (
+    // FlatLists come with so many props that you can set
+    // you can set the number of columns to 2 for example
+    <FlatList
+      // we must have a data which is an array pointing to what we wish to render
+      // i imagine that in the future, this will come from a cloud or database
+      data={CATEGORIES}
+      // the keyExtractor gets access to each specific element in the array
+      keyExtractor={(item) => {
+        return item.id;
+      }}
+      numColumns={2}
+      renderItem={renderCategoryItem}
+    />
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CategoriesScreen;
