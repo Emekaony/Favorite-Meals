343e96083b40ba547df7053082e683b1749302a4
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-06-03 00:37:44 -0700

    Not much changes done here

--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,9 +1,10 @@
+import { useLayoutEffect } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
 
-import { MEALS } from "../data/dummydata";
+import { MEALS, CATEGORIES } from "../data/dummydata";
 import MealItem from "../Components/MealItem";
 
-const MealsOverviewScreen = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
   const catId = route.params.categoryId;
 
   // go thru all the meals and keep all the meals that fall into
@@ -11,6 +12,33 @@ const MealsOverviewScreen = ({ route }) => {
   const displayedMeals = MEALS.filter((meal) => {
     return meal.categoryIds.includes(catId);
   });
+
+  /* 
+  I wanted to solve a problem where when I navigate to a different screen;
+  I wanted the header title to be set dynamically through route parameters
+  
+  I used useEffect() at first, but this only runs after your component 
+  has been rendered. This created some weird glitch and was very ugly.
+
+  The goal was to use a hook that ran simultaneously as my component
+  was being rendered. The solution turned out to be useLayoutEffect() baby!!
+
+  use -> useLayoutEffect() when you want something to happen at the 
+  same time that your component is being rendered. I could see this being expensive,
+  so it is not to be used for very heavy computations as you run the risk of slow component rendering!
+  */
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => {
+      return category.id === catId;
+    }).title;
+
+    // you can set the options for a screen inside of its own component
+    // this could be a much cleaner way to do specific things.
+    // of course, you could have default options set oon the parent stack nav
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
   // continue
   const renderMealItem = ({ item }) => {
